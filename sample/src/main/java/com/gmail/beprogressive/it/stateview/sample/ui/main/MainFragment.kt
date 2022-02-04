88c8997405d685cebd050550ef2207c1454a7741
remie <remie.lk@gmail.com>
2022-02-04 14:29:25 +0200

    ver. 1.0

---
+++ sample/src/main/java/com/gmail/beprogressive/it/stateview/sample/ui/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.gmail.beprogressive.it.stateview.sample.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gmail.beprogressive.it.stateview.sample.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+    }
+}
